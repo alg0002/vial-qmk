5c5700659ec5f554622cf0f2047cd0508df9ec07
alg <alg.info@gmail.com>
2023-11-28 23:05:05 +0900

    (teihai17n)ok

---
+++ keyboards/alg0002/teihai17n/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x53, 0x32, 0xED, 0x1A, 0x8C, 0x56, 0xB6, 0x64}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
