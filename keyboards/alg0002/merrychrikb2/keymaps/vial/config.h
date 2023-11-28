a50e2b4c50b0ccc5afbb170f592c48c73347374c
alg <alg.info@gmail.com>
2023-11-28 23:06:05 +0900

    (merrychrikb2) first commit

---
+++ keyboards/alg0002/merrychrikb2/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x6F, 0x30, 0x43, 0x69, 0x72, 0x18, 0xD0, 0xBD}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
