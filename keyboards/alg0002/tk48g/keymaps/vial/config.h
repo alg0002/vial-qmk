b07cd703ecce03bcadca9490ca2e5202b826bc24
alg <alg.info@gmail.com>
2023-12-30 23:07:04 +0900

    (tk48g) first commit

---
+++ keyboards/alg0002/tk48g/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x8F, 0x65, 0xF8, 0x2E, 0x9E, 0xEA, 0x20, 0xCF}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 4 }
