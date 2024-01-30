951e36b1afbde9137bb9862c0c25f55c8c18ec86
alg <alg.info@gmail.com>
2024-01-30 16:11:30 +0900

    Add keyboard: alg0002/teihai64

---
+++ keyboards/alg0002/teihai64/config.h
@@ -0,0 +1,12 @@
+// Copyright 2024 alg (@alg)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/* key matrix size */
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 7
+
+// wiring of each half
+#define MATRIX_ROW_PINS { GP1,  GP11, GP10, GP7,  GP8 }
+#define MATRIX_COL_PINS { GP28, GP27, GP26, GP15, GP14, GP13, GP12 }
---
+++ keyboards/alg0002/teihai64/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF5, 0x8A, 0x9F, 0xCE, 0x31, 0x0B, 0xB5, 0x70}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 6 }
