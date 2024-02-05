9557a32911d86f90412d79c7fc0dd86e4cd4e97e
alg <alg.info@gmail.com>
2024-02-06 01:22:40 +0900

    [Bugfix] Correct wrong pins

--- keyboards/alg0002/teihai48g/config.h
+++ keyboards/alg0002/teihai48g/config.h
@@ -8,5 +8,5 @@
 #define MATRIX_COLS 6
 
 // wiring of each half
-#define MATRIX_ROW_PINS { GP8,  GP11, GP10, GP7  }
-#define MATRIX_COL_PINS { GP28, GP27, GP26, GP15, GP14, GP13 }
+#define MATRIX_ROW_PINS { GP7,  GP8,  GP10, GP11 }
+#define MATRIX_COL_PINS { GP4,  GP3,  GP28, GP27, GP26, GP15 }
