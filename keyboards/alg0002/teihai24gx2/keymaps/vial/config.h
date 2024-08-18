797550f2ae74a3754f5944c2de7d833d10ec67a3
alg <alg.info@gmail.com>
2024-08-18 14:37:06 +0900

    Add keyboard: alg0002/teihai24gx2

---
+++ keyboards/alg0002/teihai24gx2/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA4, 0x02, 0x70, 0xF6, 0x3A, 0x8E, 0x37, 0x15}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 5, 6 }
