b8211b1d69d8906cbe6fa8e532a7bcc212c17601
alg <alg.info@gmail.com>
2024-10-15 22:21:50 +0900

    [Teihai24Gx2] Modify VIAL_UNLOCK_COMBO

--- keyboards/alg0002/teihai24gx2/keymaps/vial/config.h
+++ keyboards/alg0002/teihai24gx2/keymaps/vial/config.h
@@ -4,5 +4,5 @@
 
 #define VIAL_KEYBOARD_UID {0xA4, 0x02, 0x70, 0xF6, 0x3A, 0x8E, 0x37, 0x15}
 
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 5, 6 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 12 }
+#define VIAL_UNLOCK_COMBO_COLS { 0,  5 }
